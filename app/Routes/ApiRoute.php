581f8496d089ba56e92745b5ac4082bc4059af01
KarlStruv <struve3@gmail.com>
2025-03-09 21:06:01 +0200

    Added docker-compose.yml for easier setup Added route for root URL to prevent 404 errors Updated README.md

--- app/Routes/ApiRoute.php
+++ app/Routes/ApiRoute.php
@@ -5,6 +5,16 @@ declare(strict_types=1);
 global $app;
 
 use App\Controllers\UserController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response
+        ->getBody()
+        ->write('Welcome to the PHP Users and User Posts API. 
+        Please refer to "/user" and "/user/{id}/post" endpoints for the functionality.');
+    return $response;
+});
 
 $app->get('/user', [UserController::class, 'getUser']);
 
